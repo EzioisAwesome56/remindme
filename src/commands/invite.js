6c1f7316c68b9eed11c74748ab9c281746a7b2f7
Jonathan Schramm <jedijon567@gmail.com>
2018-06-13 21:38:01 -0400

    made the bot private

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,7 +1,8 @@
 exports.run = async function (Bot, msg) {
     Bot.sendMessage(msg.channel.id, { embed: {
         color: Bot.config.embedColor,
-        description: `Click [here](https://discordapp.com/oauth2/authorize?permissions=27648&scope=bot&client_id=${Bot.client.user.id}) to invite me to your server, or click [here](https://discord.gg/Yphr6WG) for an invite to RemindMeBot's support server.`
+        /*description: `Click [here](https://discordapp.com/oauth2/authorize?permissions=27648&scope=bot&client_id=${Bot.client.user.id}) to invite me to your server!`*/
+		description: `This bot is currently configured to be a private bot, and as such, cannot be invited`
     }});
 };
 
